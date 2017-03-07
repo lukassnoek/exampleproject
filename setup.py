be24caaf24d26abdfd9140064cdaa764b3b668ed
Lukas Snoek <lukassnoek@gmail.com>
2017-03-07 14:10:47 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='skbold',
+    version=0.1,
+    description='An example package for tutorial purposes',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    keywords="travis-ci, documentation, rtd, pytest",
+    url='https://github.com/lukassnoek/exampleproject',
+    author='Lukas Snoek',
+    author_email='lukassnoek@gmail.com',
+    license='MIT',
+    platforms='Linux',
+    packages=find_packages(),
+    zip_safe=False)
